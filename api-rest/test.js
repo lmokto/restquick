7c0a1f21d10cb063054d83253334a1632ca74111
lmokto <lmokto@users.noreply.github.com>
2016-10-23 21:03:16 -0300

    Update test.js

--- api-rest/test.js
+++ api-rest/test.js
@@ -1,6 +1,6 @@
 (function run() {
 	var title = document.getElementById('
 	setInterval(function(){
-		console.log(document.getElementsByClassName('logo')[0]. title)
+		console.log(document.getElementsByClassName('logo')[0].title)
 	}, 1000)
 })();
